cb2c8d212c3332b3754e5bac93adf11715b5028e
fogre <antti.kotiranta@helsinki.fi>
2018-01-24 17:57:45 +0200

    will be forgotten

--- src/components/News.js
+++ src/components/News.js
@@ -7,7 +7,10 @@ var newsStyle = {
 //asdasdasd
 class News extends Component {
 
+
   render() {
+  	var accestoken = '139858906814014|R2KakcWMqUg9u07ZlS7mT99KfZ4';
+  	var query = 'Kryptsdeath/posts?fields=id,message,link,attachments{media}&limit=5';
     return(	
     	<h1>NEWS</h1>
     );	
