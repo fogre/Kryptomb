2fe14a3acdc4738d699dfef10c08447a88196853
Fogre <antti.kotiranta@helsinki.fi>
2019-08-11 18:42:22 +0300

    management update

--- src/bodyComponents/Discography.js
+++ src/bodyComponents/Discography.js
@@ -8,16 +8,16 @@ class Discography extends Component {
 
   	let albums = disco.albums.map(album => {
   	  return <div key={album.name}>
-	    <h3>{album.name}</h3>
 	    <iframe className="BodyDiscoAlbum" 
 	    	src={album.bandcamp.src} seamless title={album.name}>
 	    	<a href={album.bandcamp.href}>{album.name}</a>
 	    </iframe>
-	    <ol className="BodyDiscoTracklist">
+      <h3>{album.name}</h3>
+	    <p className="BodyDiscoTracklist">
 	       {album.tracklist.map(track => {
 		     return <li key={track}>{track}</li>
 	       })}
-	    </ol>
+	    </p>
 	    <p>Released {album.date}</p>
   	  </div>
   	});
@@ -26,9 +26,6 @@ class Discography extends Component {
       <div>	
     	<h1>Discography</h1>
     	{albums}
-      <br/>
-      <h4><a href='https://www.metal-archives.com/bands/Krypts/3540282830' target='_blank' 
-      rel="noopener noreferrer" className="link">Full discography can be found here</a></h4>
       </div>	
     );	
   }
--- src/bodyComponents/News.js
+++ src/bodyComponents/News.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
 
-import axios from 'axios';
-import Parser from 'html-react-parser';
+import newsposts from '../data/news.json';
 
 class News extends Component {
 
-  constructor() {
-  	super();
-  	this.state = {
-  		posts: '<p>loading...</p>'
-  	}
-  }
-
-  componentDidMount() {
-  	this.getJSXFile();
-  }
-
-  getJSXFile() {
-  	axios.get('postsJSX')
-  	  .then(result => {
-  	  	this.setState({posts: result.data});
-  	  })
-  	  .catch(error => {
-  	  	console.log(error);
-  	  });
-  }
-
   render() {
+
+    let news = newsposts.posts.map(post => {
+      let postImage = (post.hasOwnProperty("image")) ? <img src={post.image} alt='' className='BodyNewsImage'/> : null;
+      return <div key={post.date}>
+        <h2>{post.date}</h2>
+        <p>{post.text}</p>
+        {postImage}
+      </div>
+    });
   	
     return(
       <div>
@@ -34,7 +21,7 @@ class News extends Component {
           <span className="ok">&#x6D;&#x6F;&#x63;&#x2E;&#x6C;&#x69;&#x61;&#x6D;&#x67;&#x40;&#x68;&#x74;&#x61;&#x65;&#x64;&#x73;&#x74;&#x70;&#x79;&#x72;&#x4B;</span>
         </p>
         <h1>News</h1>
-        {Parser(this.state.posts)}
+        {news}
 	    </div>
     );	
   }
