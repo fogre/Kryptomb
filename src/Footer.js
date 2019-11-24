22c4695519fc6e3fca08b4c90f430b9a127b8c0c
Fogre <antti.kotiranta@helsinki.fi>
2019-11-24 14:01:01 +0200

    visual and news update

--- src/Footer.js
+++ src/Footer.js
@@ -8,6 +8,12 @@ class Footer extends Component {
   	  	<br/>
   	    <p>Logo and background art by T.Ketola. Site by fogre.
   	    <br/>All rights reserved.</p>
+  	    <div className="FooterLinks">
+  	    	<a href='https://facebook.com/kryptsdeath' target='_blank' rel="noopener noreferrer"
+          		className="NavigationLink">Facebook</a>
+          	<a href='https://instagram.com/kryptsdeath' target='_blank' rel="noopener noreferrer"
+          className="NavigationLink">Instagram</a>
+        </div>  
   	  </div>
   	);
   }
--- src/bodyComponents/Discography.js
+++ src/bodyComponents/Discography.js
@@ -7,25 +7,30 @@ class Discography extends Component {
   render() {
 
   	let albums = disco.albums.map(album => {
+      let albumCover = album.bandcamp ?
+        ( <iframe className="BodyDiscoAlbum" 
+            src={album.bandcamp.src} seamless title={album.name}>
+            <a href={album.bandcamp.href}>{album.name}</a>
+          </iframe>
+        ) : ( <img src="/open.jpeg" alt="open" className="BodyDiscoAlbum"/> );
+
   	  return <div key={album.name}>
-	    <iframe className="BodyDiscoAlbum" 
-	    	src={album.bandcamp.src} seamless title={album.name}>
-	    	<a href={album.bandcamp.href}>{album.name}</a>
-	    </iframe>
-      <h3>{album.name}</h3>
-	    <p className="BodyDiscoTracklist">
-	       {album.tracklist.map(track => {
-		     return <li key={track}>{track}</li>
-	       })}
-	    </p>
-	    <p>Released {album.date}</p>
+  	    {albumCover}
+        <h3>{album.name}</h3> 
+  	    <p className="BodyDiscoTracklist">
+  	       {album.tracklist.map(track => {
+  		     return <li key={track}>{track}</li>
+  	       })}
+  	    </p>
+  	    <p>Released {album.date}</p>
+        <br/>
   	  </div>
   	});
 
     return(	
       <div>	
-    	<h1>Discography</h1>
-    	{albums}
+    	 <h1>Discography</h1>
+    	 {albums}
       </div>	
     );	
   }
